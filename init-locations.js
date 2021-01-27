b61b0c147dfdcbe780d6c80bbb5de19d0921ee72
annie0sc <annie0sc@gmail.com>
2021-01-26 22:01:37 -0600

    Added 2 locations in init-locations.js

---
+++ init-locations.js
@@ -0,0 +1,23 @@
+export default [
+    {
+      Name: 'Horizons One',
+      North: 40.344208,
+      West: -94.891494,
+      South: 40.343652,
+      East: -94.8884,
+      Notes: 'circle',
+      Attachments: '',
+      'Quest-Location': 'Horizons One',
+    },
+    {
+      Name: 'Horizons Two',
+      North: 40.343579,
+      West: -94.89084,
+      South: 40.343268,
+      East: -94.8884,
+      Notes: 'heading south',
+      Attachments: '',
+      'Quest-Location': 'Horizons Two',
+    },
+    
+  ];
